2ba18b0e81b3de4899d0faa37c3b021ee3ba4c21
D.G.S.S.Dhananjana <149720690+dhananjana998@users.noreply.github.com>
2025-09-13 11:08:28 +0530

    Add files via upload

---
+++ argument.py
@@ -0,0 +1,13 @@
+def total(*marks): #valuse are storewd in tuple
+    total=0
+    for mark in marks:
+        total +=mark
+    print(" Total: ",total)
+total(87,56,54)
+
+""" OUTPUT:
+
+ Total:  197
+
+"""
+
